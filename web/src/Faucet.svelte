6371fc333a0bee372e19540746252726f89b253f
Sitthiphorn Anthawonksa <sitthiphornanthawonksa@Sitthiphorns-MacBook-Pro.local>
2022-07-07 12:29:26 +0700

    change txt

--- web/src/Faucet.svelte
+++ web/src/Faucet.svelte
@@ -86,7 +86,7 @@
       <div class="container has-text-centered">
         <div class="column is-6 is-offset-3">
           <h1 class="title">
-            Receive {faucetInfo.payout} ETH per request
+            Receive {faucetInfo.payout} JFIN per request
           </h1>
           <h2 class="subtitle">
             Serving from {faucetInfo.account}
